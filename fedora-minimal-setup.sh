e2fee1885240db0cd9e5036acc82e77f28e5b065
Matheus Guedes <mguedes@arclight.dev>
2020-11-18 19:41:04 -0300

    Update script

--- fedora-minimal-setup.sh
+++ fedora-minimal-setup.sh
@@ -3,7 +3,17 @@
 if ! [ $(id -u) = 0 ]; then
   echo "This script must be run as sudo or root, try again..."
   exit 1
-fi
+fi;
+
+# Enable RPMFusion
+dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm;
+
+# Enable VSCode
+rpm --import https://packages.microsoft.com/keys/microsoft.asc;
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo;
+
+# Refresh repos
+dnf check-update;
 
 # Install Plasma5 + Hardware Support
 dnf install -y \
@@ -76,7 +86,7 @@ dnf install -y \
   setroubleshoot \
   sni-qt \
   system-config-language \
-  xorg-x11-drv-libinput
+  xorg-x11-drv-libinput;
 
   
 # Install useful KDE applications
@@ -89,33 +99,25 @@ krita \
 gwenview \
 spectacle \
 okular \
-kvantum
-
-# Enable RPMFusion
-dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-
-# Enable VSCodium
-rpm --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg 
-printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=gitlab.com_paulcarroty_vscodium_repo\nbaseurl=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/repos/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg" | tee -a /etc/yum.repos.d/vscodium.repo
+kvantum;
 
 # Install Flatpak + Flathub
-dnf install -y flatpak
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+dnf install -y flatpak;
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo;
 
 # Install Printing Support
-dnf install -y @"Printing Support"
+dnf install -y @"Printing Support";
 
 # Install Fonts
 dnf install -y \
 google-noto-emoji-fonts \
-jetbrains-mono-fonts
+jetbrains-mono-fonts;
 
 # Install everyday applications
 dnf install -y \
 chromium-browser-privacy \
-codium \
 compat-ffmpeg28 \
-discord \
+code \
 ffmpeg \
 firefox \
 libreoffice \
@@ -125,15 +127,18 @@ torbrowser-launcher \
 vim \
 wine \
 winetricks \
-youtube-dl
+youtube-dl;
+
+flatpak install -y \
+com.discordapp.Discord;
 
 # Purge PackageKit
-dnf remove -y PackageKit
+dnf remove -y PackageKit;
 
 # Fix Plymouth
-plymouth-set-default-theme -R bgrt
+plymouth-set-default-theme -R bgrt;
 
 # Enable SDDM and start it up
-systemctl enable sddm
-systemctl set-default graphical.target
-systemctl start graphical.target
+systemctl enable sddm;
+systemctl set-default graphical.target;
+systemctl start graphical.target;
